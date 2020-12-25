688784766a8dea84b229ac3710636bf08324447a
Joachim Metz <joachim.metz@gmail.com>
2020-12-25 06:45:42 +0100

    Applied updates

--- libftxf/libftxf_extern.h
+++ libftxf/libftxf_extern.h
@@ -30,7 +30,7 @@
 
 #include <libftxf/extern.h>
 
-#if defined( __CYGWIN__ )
+#if defined( __CYGWIN__ ) || defined( __MINGW32__ )
 #define LIBFTXF_EXTERN_VARIABLE	extern
 #else
 #define LIBFTXF_EXTERN_VARIABLE	LIBFTXF_EXTERN
